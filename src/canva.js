ccde6645197c954fa40bd4e38df04629c2abe100
Nikita Machekhin <nicitamac@gmail.com>
2020-10-01 00:20:38 +0300

    Add mode select buttons layout

--- src/canva.js
+++ src/canva.js
@@ -30,7 +30,20 @@ const canva = new Canva(document.getElementsByTagName('canvas')[0]);
 
 canva.render()
 canva.init();
-window.addEventListener(`resize`, event => {
+window.addEventListener('resize', event => {
   canva.render();
   canva.init();
 }, false);
+
+const myselfModeButton = document.getElementById('myself-mode');
+const computerModeButton = document.getElementById('computer-mode')
+
+myselfModeButton.onclick = () => {
+  myselfModeButton.classList.add('selected')
+  computerModeButton.classList.remove('selected');
+}
+
+computerModeButton.onclick = () => {
+  computerModeButton.classList.add('selected')
+  myselfModeButton.classList.remove('selected');
+}
