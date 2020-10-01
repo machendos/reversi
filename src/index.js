b99888d2082d7e901ecd8fabcf34fb5eba6e68e5
Nikita Machekhin <nicitamac@gmail.com>
2020-10-02 02:33:09 +0300

    Update main file

--- src/index.js
+++ src/index.js
@@ -1,41 +1,23 @@
 'use strict';
 
-const Canva = require(__dirname + '/canva.js');
+const ReversiController = require(__dirname + '/controller.js');
+const ReversiModel = require(__dirname + '/model.js');
 
-const canva = new Canva(document.getElementsByTagName('canvas')[0]);
+const controller = new ReversiController(
+  document.getElementsByTagName('canvas')[0]
+);
 
-canva.render();
-canva.init();
+controller.render();
+controller.init();
+
 window.addEventListener(
   'resize',
   () => {
-    canva.render();
-    canva.init();
+    controller.render();
+    controller.init();
   },
   false
 );
 
-const myselfModeButton = document.getElementById('myself-mode');
-const computerModeButton = document.getElementById('computer-mode');
-
-myselfModeButton.onclick = () => {
-  myselfModeButton.classList.add('selected');
-  computerModeButton.classList.remove('selected');
-};
-
-computerModeButton.onclick = () => {
-  computerModeButton.classList.add('selected');
-  myselfModeButton.classList.remove('selected');
-};
-
-canva.put(2, 2, 'black');
-
-canva.changeColor(2, 2, 'black', 'white');
-canva.changeColor(3, 3, 'black', 'white');
-setTimeout(() => {
-  canva.changeColor(5, 5, 'black', 'white');
-}, 500);
-
-canva.onBoardClick(({ row, column }) => {
-  console.log(row, column);
-});
+const model = new ReversiModel(controller);
+model.init();
