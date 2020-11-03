f92175828a91ccf36fcfc064620d11ea89d69000
kuvichkamaksim <kuvichkamaksim@gmail.com>
2020-11-03 03:45:32 +0200

    add some spaces to visually merge code into logic blocks; add game finish logic; handle cases when both players don't have available steps

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -7,6 +7,7 @@ const {
   COMPUTER_MODE,
   USER_MODE,
   WHITE_CHIP_MATRIX_VALUE,
+  RESULT_TEXTS,
 } = require(__dirname + '/../utils/constants');
 
 const myselfModeButton = document.getElementById('myself-mode');
@@ -14,11 +15,14 @@ const computerModeButton = document.getElementById('computer-mode');
 const currPlayerMarker = document.getElementById('curr-player');
 const blackChipsCounter = document.getElementById('black-counter');
 const whiteChipsCounter = document.getElementById('white-counter');
+const gameResultText = document.getElementById('game-result-text');
 
 class ReversiController extends Canva {
   constructor(canvasContainer) {
     super(canvasContainer);
+    this.gameResult = '';
   }
+
   onModeChange(listener) {
     myselfModeButton.onclick = () => {
       myselfModeButton.classList.add('selected');
@@ -35,18 +39,34 @@ class ReversiController extends Canva {
 
   setCurrPlayer(player) {
     const { fill } = styles[player];
-    console.log(fill);
     currPlayerMarker.style.setProperty('background', fill);
   }
 
   chipCounterIncrement(player, incrementCount) {
+    const oldWhiteValue = parseInt(whiteChipsCounter.innerText);
+    const oldBlackValue = parseInt(blackChipsCounter.innerText);
+
     if (player === WHITE_CHIP_MATRIX_VALUE) {
-      whiteChipsCounter.innerText =
-        parseInt(whiteChipsCounter.innerText) + incrementCount;
+      whiteChipsCounter.innerText = oldWhiteValue + incrementCount;
+      this.setLeader(oldWhiteValue + incrementCount, oldBlackValue);
     } else {
-      blackChipsCounter.innerText =
-        parseInt(blackChipsCounter.innerText) + incrementCount;
+      blackChipsCounter.innerText = oldBlackValue + incrementCount;
+      this.setLeader(oldWhiteValue, oldBlackValue + incrementCount);
     }
+  }
+
+  setLeader(whiteScore, blackScore) {
+    if (whiteScore > blackScore) {
+      this.gameResult = 'white';
+    } else if (blackScore > whiteScore) {
+      this.gameResult = 'black';
+    } else {
+      this.gameResult = 'draw';
+    }
+  }
+
+  handleGameFinish() {
+    gameResultText.innerText = RESULT_TEXTS[this.gameResult];
   }
 }
 
